92fad48c87efc9c2cd2ebbd3fd73553598bb5c6e
Riju-Saha <21.sahasoumyajit@gmail.com>
2025-10-16 10:45:08 +0530

    sellers api done

--- app/sellers/Men/Jeans/page.tsx
+++ app/sellers/Men/Jeans/page.tsx
@@ -206,11 +206,13 @@ export default function Jeans() {
     setIsDropdownOpen(false);
     alert(`Routing to ${user!.name}'s Seller Profile Page!`);
   };
+
   const handleLogout = () => {
     setIsDropdownOpen(false);
     logout();
     router.push('/');
   };
+  
   const CartIcon = () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
